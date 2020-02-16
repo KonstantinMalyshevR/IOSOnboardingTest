8ace7d6eecdb8b6e8222c9f52df7de23aed19e1e
Konstantin Malyshev <rmkonstantin@gmail.com>
2020-02-16 12:08:23 +0900

    Initial Commit

---
+++ iOSTestOnboarding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSTestOnboarding
+//
+//  Created by Konstantin Malyshev on 16.02.2020.
+//  Copyright © 2020 Konstantin Malyshev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
